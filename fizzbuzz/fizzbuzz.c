f1f7849380171894d1c2f146515278129fc4b6d2
pmoreira <pmoreira@student.42lisboa.com>
2024-09-09 16:18:26 +0100

    fizzbuzz

---
+++ fizzbuzz/fizzbuzz.c
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fizzbuzz.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pmoreira <pmoreira@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/09 16:05:06 by pmoreira          #+#    #+#             */
+/*   Updated: 2024/09/09 16:18:06 by pmoreira         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putnbr(int nb);
+
+void	ft_putnbr(int nb)
+{
+	char	dec;
+	char	unit;
+
+	dec = (nb / 10) + '0';
+	unit = (nb % 10) + '0';
+	if (nb >= 0 && nb <= 9)
+	{
+		write(1, &unit, 1);
+	}
+	else
+	{
+		write(1, &dec, 1);
+		write(1, &unit, 1);
+	}
+}
+
+int	main(void)
+{
+	int	i;
+
+	i = 1;
+	while (i <= 100)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+			write(1, "fizzbuzz", 8);
+		else if (i % 3 == 0)
+			write(1, "fizz", 4);
+		else if (i % 5 == 0)
+			write(1, "buzz", 4);
+		else
+			ft_putnbr(i);
+		write(1, "\n", 1);
+		i++;
+	}
+	return (0);
+}
