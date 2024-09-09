c489e0ad8c8f11112f8188ba2538d9d8d816b2cb
pmoreira <pmoreira@student.42lisboa.com>
2024-09-09 15:25:50 +0100

    ft_print_numbers

---
+++ ft_print_numbers/ft_print_numbers.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_numbers.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pmoreira <pmoreira@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/09 15:22:04 by pmoreira          #+#    #+#             */
+/*   Updated: 2024/09/09 15:25:25 by pmoreira         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_numbers(void);
+
+void	ft_print_numbers(void)
+{
+	int	i;
+
+	i = '0';
+	while (i <= '9')
+	{
+		write(1, &i, 1);
+		i++;
+	}
+}
+/*
+int	main(void)
+{
+	ft_print_numbers();
+	return (0);
+}
+*/
