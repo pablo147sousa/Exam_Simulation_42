b2ab31220faf04977e53807f3944f5d1e0d81594
pmoreira <pmoreira@student.42lisboa.com>
2024-09-09 15:08:46 +0100

    norminette hello && maff_alpha

--- maff_alpha/maff_alpha.c
+++ maff_alpha/maff_alpha.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   hello.c                                            :+:      :+:    :+:   */
+/*   maff_alpha.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: pmoreira <pmoreira@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/09/07 17:51:11 by pmoreira          #+#    #+#             */
-/*   Updated: 2024/09/09 15:07:45 by pmoreira         ###   ########.fr       */
+/*   Created: 2024/09/09 14:58:58 by pmoreira          #+#    #+#             */
+/*   Updated: 2024/09/09 15:07:59 by pmoreira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,18 @@
 
 int	main(void)
 {
-	write(1, "Hello World!\n", 13);
+	char	odd;
+	char	even;
+
+	odd = 'B';
+	even = 'a';
+	while (odd <= 'Z' && even <= 'y')
+	{
+		write(1, &even, 1);
+		write(1, &odd, 1);
+		even += 2;
+		odd += 2;
+	}
+	write(1, "\n", 1);
 	return (0);
 }
